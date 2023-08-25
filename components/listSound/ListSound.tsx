d1edfe4560417425f65a545a0cb9ddbbb9359294
NamLTN <nhatnam11052000@gmail.com>
2023-08-25 18:55:24 +0700

    update audio player

--- components/listSound/ListSound.tsx
+++ components/listSound/ListSound.tsx
@@ -3,14 +3,25 @@ import { Tag } from '@/pages/index';
 import { CircularProgress } from '@mui/material';
 import * as React from 'react';
 import style from "./ListSound.module.scss";
-import ReactAudioPlayer  from "react-audio-player"
-
+import { createTheme, ThemeProvider, useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import SkipNextIcon from '@mui/icons-material/SkipNext';
+import PauseIcon from '@mui/icons-material/Pause'
+import LinearProgress from '@mui/material/LinearProgress';
+import Slider from '@mui/material/Slider';
 export interface IAppProps {
     playlist:Tag,
     
 }
 
-export interface filmDetails {
+export interface audioDetails {
     itune_link: string,
     name: string,
     artist: string
@@ -22,12 +33,60 @@ interface SoundDetails{
 
 interface AudioPlayerProps {
     src: string;
-    onPlay: (e:HTMLAudioElement | null) => void;
+    onPlay: (e:HTMLAudioElement | null) => void,
+    audio?:audioDetails
 }
 
-const AudioPlayer: React.FC<AudioPlayerProps> = ({ src, onPlay }) => {
+interface MediaControlProp {
+    onNext?: () => void,
+    onPrevious?: () => void,
+    isPlaying?:boolean,
+    setPlaying:() => void,
+    time:number,
+    audio?:audioDetails,
+    setCurrentTime:(time:number | number[])=> void
+}
+
+export const MediaControlCard:React.FC<MediaControlProp> = ({isPlaying,setPlaying,time,audio,setCurrentTime}) => {
+    const theme = useTheme();
+  
+    return (
+      <Card sx={{ display: 'flex',marginTop:"12px", width:"100%", maxWidth:"1200px"}}>
+        <Box sx={{ display: 'flex', flexDirection: 'column' , width:"100%"}}>
+          <CardContent sx={{ flex: '1 0 auto' }}>
+            <Typography component="div" variant="h5">
+              {audio?.name}
+            </Typography>
+            <Typography variant="subtitle1" color="text.secondary" component="div">
+            {audio?.artist}
+            </Typography>
+          </CardContent>
+          <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
+            <IconButton aria-label="play/pause" onClick={setPlaying}>
+              {isPlaying ? <PauseIcon sx={{ height: 38, width: 38 }}/> : <PlayArrowIcon sx={{ height: 38, width: 38 }} />}
+            </IconButton>
+            <Box sx={{ width: '100%', maxWidth:"300px",marginTop:"8px" }}>
+                <Slider value={time*100} aria-label="Temperature" color="secondary" onChange={(e:Event, newvalue:number | number[])=>{setCurrentTime(newvalue); }}/>
+            </Box>
+          </Box>
+        </Box>
+      </Card>
+    );
+}
+const darkTheme = createTheme ({
+    palette: {
+      mode: 'dark',
+      primary: {
+        main: '#1976d2',
+      },
+    },
+  });
+const AudioPlayer: React.FC<AudioPlayerProps> = ({ src, onPlay,audio }) => {
+
+
     const [isPlaying, setIsPlaying] = React.useState(false);
-    const audioRef = React.createRef<HTMLAudioElement>();
+    let audioRef = React.createRef<HTMLAudioElement>();
+    const [time,setTime] = React.useState(0)
     
     const togglePlay = () => {
         if (isPlaying) {
@@ -39,24 +98,50 @@ const AudioPlayer: React.FC<AudioPlayerProps> = ({ src, onPlay }) => {
         setIsPlaying(!isPlaying);
     };
     
+    function setCurrentTime(time:number | number[]) {
+        if (typeof time == "number") {
+            const totalTime = audioRef.current?.duration ? audioRef.current?.duration : 1
+            if (audioRef.current){
+                audioRef.current.currentTime = (time/100)*totalTime
+            }
+            setTime(time/100)
+            audioRef.current!.play()
+            setIsPlaying(true)
+        }
+        
+    }
+
+    function setTimeInterval(){
+        const currentTime = audioRef.current?.currentTime ? audioRef.current?.currentTime : 0
+        const totalTime = audioRef.current?.duration ? audioRef.current?.duration : 1
+        if  (currentTime/totalTime == 1){
+            setTime(0)
+        } else {
+            setTime(currentTime/totalTime) 
+        }
+        
+    }
+    
     return (
-        <div>
-        <audio ref={audioRef} src={src} onPause={()=>{setIsPlaying(false)}}/>
-        <button onClick={togglePlay}>
-            {isPlaying ? 'Pause' : 'Play'}
-        </button>
-        </div>
+        <ThemeProvider theme={darkTheme}>
+            <div style={{ width:"100%",display:"flex",alignItems:"center"}}>
+                <audio ref={audioRef} src={src} onPause={()=>{setIsPlaying(false)}} onLoadStart={()=>{setIsPlaying(false); setTime(0)}} onTimeUpdate={()=>setTimeInterval()}/>
+                <MediaControlCard isPlaying={isPlaying} setPlaying={togglePlay} time={time} audio={audio} setCurrentTime={setCurrentTime}/>
+            </div>
+        </ThemeProvider>
+        
     );
 };
 
 export default function ListSound (props: IAppProps) {
     const [isLoading, setIsLoading] = React.useState(true);
-    let initState:filmDetails[] = []
+    let initState:audioDetails[] = []
     const [filmDetails, setFilmDetails] = React.useState(initState)
     const [activePlayer, setActivePlayer] = React.useState<HTMLAudioElement | null>(null)
-  
+    const [listAudioElm, setListAudioElm] = React.useState<HTMLAudioElement | null[]>([])
     const handlePlay = (playerRef: HTMLAudioElement | null) => {
-      activePlayer ? activePlayer.pause() : ()=>{};
+      activePlayer != playerRef ? activePlayer?.pause() : ()=>{};
+      activePlayer != playerRef ? activePlayer?.load() : ()=>{};
       setActivePlayer(playerRef);
     };
     const fetchData = async ()=>{
@@ -144,6 +229,7 @@ export default function ListSound (props: IAppProps) {
                     <AudioPlayer
                         key={index}
                         src={el.itune_link}
+                        audio={el}
                         onPlay={handlePlay}
                     /> 
                 ))
