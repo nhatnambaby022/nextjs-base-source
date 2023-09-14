2144d98c60c5ed97699c6d8a70569d09476922e2
vietanhyoyo <vanhyoyo@gmail.com>
2023-09-14 21:57:40 +0700

    done banner

--- components/banner/Banner.tsx
+++ components/banner/Banner.tsx
@@ -1,35 +1,81 @@
 import React, { useRef, useState, useEffect } from "react";
-import { IconButton } from "@mui/material";
+import { IconButton, Button } from "@mui/material";
 import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
 import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+import HeadphonesIcon from "@mui/icons-material/Headphones";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 import style from "./Banner.module.scss";
-import { Tag } from "@/pages/index";
+
+export interface Film {
+  id: string;
+  name: string;
+  thumbnail: string;
+  backdrop: string;
+  description: string;
+  slug?: string;
+  soundtrack_count: number;
+  author?: string;
+  type?: number;
+}
 
 interface BannerProps {
-    listData: Tag[];
-  }
+  listData: Film[];
+}
 
 const Banner: React.FC<BannerProps> = ({ listData }) => {
   const [active, setActive] = useState(0);
   const listDivRef = useRef(null);
 
-  const bannerImages = listData.map(e => e.thumbnail) 
+  const banImgs = [listData[0], listData[1], listData[2]];
 
   const changeNext = () => {
-    setActive((prevActive) => (prevActive + 1) % bannerImages.length);
+    setActive((prevActive) => (prevActive + 1) % banImgs.length);
   };
-  
+
   const changePrev = () => {
     setActive((prevActive) =>
-      prevActive === 0 ? bannerImages.length - 1 : prevActive - 1
+      prevActive === 0 ? banImgs.length - 1 : prevActive - 1
     );
   };
-  
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      changeNext();
+    }, 5000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [active]);
+
   return (
     <div className={style.slider} id="slider">
       <div className={style.list} ref={listDivRef}>
-        <div className="banner-item">
-          <img src={bannerImages[active]} alt={`Banner ${active}`} />
+        <div className={style.bannerItem}>
+          <div className={style.content}>
+            <p>{banImgs[active].description ?? ""}</p>
+            <div className={style.row}>
+              <div className={style.column}>
+                <HeadphonesIcon className={style.banIcon} />
+                <span>{banImgs[active].soundtrack_count} songs</span>
+              </div>
+              <div>
+                <Button
+                  startIcon={<PlayArrowIcon />}
+                  className={style.banButton}
+                  variant="contained"
+                >
+                  Play
+                </Button>
+              </div>
+            </div>
+          </div>
+          <div className={style.imgBefore}></div>
+            <img
+              className={style.img}
+              src={banImgs[active] ? banImgs[active].backdrop : ""}
+              alt={`Banner ${active}`}
+            />
         </div>
       </div>
       <div className={style.buttons}>
@@ -41,8 +87,8 @@ const Banner: React.FC<BannerProps> = ({ listData }) => {
         </IconButton>
       </div>
       <ul className={style.dots}>
-        {listData.map((e, index) => {
-            return <li className={active == index ? style.active : ""}></li>
+        {banImgs.map((e, index) => {
+          return <li className={active == index ? style.active : ""}></li>;
         })}
       </ul>
     </div>
