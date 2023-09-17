1829fba8164353ba5a3e340f313748d1dc79fed9
NamLTN <74360788+nhatnambaby022@users.noreply.github.com>
2023-09-17 17:07:04 +0700

    banner fix

--- components/banner/Banner.tsx
+++ components/banner/Banner.tsx
@@ -75,7 +75,9 @@ const Banner: React.FC<BannerProps> = ({ listData }) => {
           </div>
           <div className={style.imgBefore}></div>
             <img
-              style={{objectFit:"contain"}}
+              style={{
+                objectFit:"contain",
+              }}
               className={style.img}
               src={banImgs[active] ? banImgs[active].backdrop : ""}
               alt={`Banner ${active}`}
