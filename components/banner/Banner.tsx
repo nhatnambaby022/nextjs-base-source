1df63ca2767b9bc3b9f812779e27ca4c1074ed66
vietanhyoyo <vanhyoyo@gmail.com>
2023-10-22 11:41:44 +0700

    update  banner

--- components/banner/Banner.tsx
+++ components/banner/Banner.tsx
@@ -1,104 +1,127 @@
-import React, { useRef, useState, useEffect } from "react";
-import { IconButton, Button } from "@mui/material";
-import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
-import NavigateNextIcon from "@mui/icons-material/NavigateNext";
-import HeadphonesIcon from "@mui/icons-material/Headphones";
-import PlayArrowIcon from "@mui/icons-material/PlayArrow";
-import style from "./Banner.module.scss";
-import Link from "next/link";
+import React, { useRef, useState, useEffect } from 'react'
+import { IconButton, Button } from '@mui/material'
+import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore'
+import NavigateNextIcon from '@mui/icons-material/NavigateNext'
+import HeadphonesIcon from '@mui/icons-material/Headphones'
+import PlayArrowIcon from '@mui/icons-material/PlayArrow'
+import style from './Banner.module.scss'
+import Link from 'next/link'
 
 export interface Film {
-  id: string;
-  name: string;
-  thumbnail: string;
-  backdrop: string;
-  description: string;
-  slug?: string;
-  soundtrack_count: number;
-  author?: string;
-  type?: number;
+  id: string
+  name: string
+  thumbnail: string
+  backdrop: string
+  banner_image: string
+  description: string
+  slug?: string
+  soundtrack_count: number
+  author?: string
+  type?: number
 }
 
 interface BannerProps {
-  listData: Film[];
+  listData: Film[]
 }
 
 const Banner: React.FC<BannerProps> = ({ listData }) => {
-  const [active, setActive] = useState(0);
-  const banImgs = [listData[0], listData[1], listData[2]];
-  const listDivRef = useRef(null);
+  const [active, setActive] = useState(0)
+  const banImgs = [listData[0], listData[1], listData[2]]
+  const listDivRef = useRef(null)
 
   const changeNext = () => {
-    setActive((prevActive) => (prevActive + 1) % banImgs.length);
-  };
+    setActive((prevActive) => (prevActive + 1) % banImgs.length)
+  }
 
   const changePrev = () => {
     setActive((prevActive) =>
       prevActive === 0 ? banImgs.length - 1 : prevActive - 1
-    );
-  };
+    )
+  }
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      changeNext();
-    }, 5000);
+      changeNext()
+    }, 5000)
 
     return () => {
-      clearTimeout(timer);
-    };
-  }, [active]);
+      clearTimeout(timer)
+    }
+  }, [active])
 
-  return <>{banImgs ?
-    <div className={style.slider} id="slider">
-      <div className={style.list} ref={listDivRef}>
-        <div className={style.bannerItem}>
-          <div className={style.content}>
-            <h3>{banImgs[active]?.name ?? ""}</h3>
-            <p>{banImgs[active]?.description ?? ""}</p>
-            <div className={style.row}>
-              <div className={style.column}>
-                <HeadphonesIcon className={style.banIcon} />
-                <span>{banImgs[active]?.soundtrack_count} songs</span>
+  return (
+    <>
+      {banImgs ? (
+        <div className={style.slider} id="slider">
+          <div className={style.list} ref={listDivRef}>
+            <div className={style.bannerItem}>
+              <div className={style.content}>
+                <h3>{banImgs[active]?.name ?? ''}</h3>
+                <p>{banImgs[active]?.description ?? ''}</p>
+                <div className={style.row}>
+                  <div className={style.column}>
+                    <HeadphonesIcon className={style.banIcon} />
+                    <span>{banImgs[active]?.soundtrack_count} songs</span>
+                  </div>
+                  <div>
+                    <Link href={`/movies/${banImgs[active]?.slug}`}>
+                      <Button
+                        startIcon={<PlayArrowIcon />}
+                        className={style.banButton}
+                        variant="contained"
+                      >
+                        Play
+                      </Button>
+                    </Link>
+                  </div>
+                </div>
               </div>
-              <div>
-                <Link href={`/movies/${banImgs[active]?.slug}`}>
-                  <Button
-                    startIcon={<PlayArrowIcon />}
-                    className={style.banButton}
-                    variant="contained"
-                  >
-                    Play
-                  </Button>
-                </Link>
-              </div>
+              <div className={style.imgBefore}></div>
+              <img
+                className={style.img}
+                src={
+                  banImgs[active]
+                    ? banImgs[active].banner_image
+                      ? banImgs[active].banner_image
+                      : banImgs[active].backdrop
+                    : ''
+                }
+                alt={`Banner ${active}`}
+              />
             </div>
           </div>
-          <div className={style.imgBefore}></div>
-            <img
-              style={{
-                objectFit:"contain",
-              }}
-              className={style.img}
-              src={banImgs[active] ? banImgs[active].backdrop : ""}
-              alt={`Banner ${active}`}
-            />
+          <div className={style.buttons}>
+            <IconButton
+              className={style.iconBtn}
+              id="prev"
+              onClick={changePrev}
+            >
+              <NavigateBeforeIcon />
+            </IconButton>
+            <IconButton
+              className={style.iconBtn}
+              id="next"
+              onClick={changeNext}
+            >
+              <NavigateNextIcon />
+            </IconButton>
+          </div>
+          <ul className={style.dots}>
+            {banImgs.map((e, index) => {
+              return (
+                <li
+                  key={index}
+                  className={active == index ? style.active : ''}
+                ></li>
+              )
+            })}
+          </ul>
         </div>
-      </div>
-      <div className={style.buttons}>
-        <IconButton className={style.iconBtn} id="prev" onClick={changePrev}>
-          <NavigateBeforeIcon />
-        </IconButton>
-        <IconButton className={style.iconBtn} id="next" onClick={changeNext}>
-          <NavigateNextIcon />
-        </IconButton>
-      </div>
-      <ul className={style.dots}>
-        {banImgs.map((e, index) => {
-          return <li key={index} className={active == index ? style.active : ""}></li>;
-        })}
-      </ul>
-    </div>
-   : <div />}</>
-};
+      ) : (
+        <div />
+      )}
+    </>
+  )
+}
 
-export default Banner;
+export default Banner
