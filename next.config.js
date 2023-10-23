a3d25de152b468436f529bb570b2d26920c044ac
nhatnambaby022 <141220469+ThuyAnh082023@users.noreply.github.com>
2023-10-23 08:25:01 +0700

    change api link

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   env:{
     // API_URL:"https://boxtube.net/api",
-    API_URL:"http://45.79.198.164:5000",
+    API_URL:"https://api.popcornsound.com/",
     // API_URL:"http://localhost:5000",
     THE_MOVIE_DB:"https://api.themoviedb.org/3",
     API_KEY:"83cf1b15a4ed3c13cf769946a81f92ee",
